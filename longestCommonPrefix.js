43b6f24ae1e3be452eac4eabf4ce8e7857b950c5
DineshDhanoki <167537800+DineshDhanoki@users.noreply.github.com>
2024-10-28 03:50:33 +0530

    Add files via upload

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -1,22 +1,22 @@
-function longestCommonPrefix(strs) {
-    if (strs.length === 0){
+function longestCommonPrefix(strs){
+    if(strs.length === 0){
         return ''
     }
 
-    let prefix = strs[0]
+    let prefix = strs[0].toLowerCase()
 
-    for(i=0; i<strs.length; i++){
-        while(strs[i].indexOf(prefix) !== 0){
+    for (let i=1; i<strs.length; i++){
+        while (strs[i].toLowerCase().indexOf(prefix) !== 0){
             prefix = prefix.slice(0,-1)
 
-            if(prefix === 0){
-                return ''
-            }
+                if(prefix === ''){
+                    return ''
+                }
         }
     }
     return prefix
 }
 
-const strs = ['dinesh', 'diplo', 'diplodocus']
+const strs = ['dinesh', 'diplo', 'DIPLODOCUS']
 const result = longestCommonPrefix(strs)
 console.log(result)
